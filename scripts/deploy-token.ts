02b7c30f0879621fcec8cdf97e55746365d09b80
Alex Tkachenko <skyalexx@gmail.com>
2021-09-26 02:28:36 +0300

    Flattern contracts, deploy token script

--- scripts/deploy-token.ts
+++ scripts/deploy-token.ts
@@ -1,7 +1,21 @@
 import { ethers } from "hardhat"
+const { BigNumber } = require("@ethersproject/bignumber");
 
 async function main() {
-  // TODO: deploy ERC20 token
+  const deployer = await (ethers.provider.getSigner()).getAddress()
+
+  const XCoin = await ethers.getContractFactory('XCoin')
+  console.log('Deploying XCoin...')
+  const xcoin = await XCoin.deploy()
+  await xcoin.deployed()
+  console.log('XCoin deployed:', xcoin.address)
+
+  const tokenAmount = BigNumber.from('1000000000000000000000')
+  xcoin.mint(tokenAmount, deployer, { gasLimit: 500000 })
+  console.log('minted', tokenAmount.toString())
+
+  const balance = await xcoin.balanceOf(deployer)
+  console.log('deployer balance:', balance)
 }
 
 main()
